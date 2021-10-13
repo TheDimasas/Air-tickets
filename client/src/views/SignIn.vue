2bc39bf0a310a37fc9b97d5bb144af171957f440
Dmytro Yefremov <dimochka.efremov@gmail.com>
2021-10-13 22:21:59 +0300

    Update client

---
+++ client/src/views/SignIn.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="auth">
+    <SignIn />
+  </div>
+</template>
+
+<script lang="ts">
+import SignIn from '../components/auth/SignIn.vue';
+
+export default {
+  components: {
+    SignIn,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.auth {
+  padding-top: 15vh;
+}
+</style>
